382299fb845e3549548fc3eae843fb397cafbe7a
gowtham-ggg <gowthamvinoth2015@gmail.com>
2025-02-04 20:10:26 +0530

    updated projects

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,49 +1,128 @@
-import project1_img from '../assets/project1.png'
-import project3_img from '../assets/project3.png'
-import imagify from "../assets/imagify.png"
-import taxi from "../assets/taxi.png"
-import realEstate from "../assets/realestate.png"
-import portfolio from "../assets/portfolio.png"
+import project1_img from '../assets/project1.png';
+import project3_img from '../assets/project3.png';
+import imagify from "../assets/imagify.png";
+import taxi from "../assets/taxi.png";
+import realEstate from "../assets/realestate.png";
+import portfolio from "../assets/portfolio.png";
+import qrgen from "../assets/project2.png";
+import doctor from "../assets/prescripto.png";
+import bmi from "../assets/project4.png";
+import movieRating from "../assets/project6.png";
+import simpleProfile from "../assets/project5.png";
+import passGen from "../assets/passGen.png";
+import jsminiProj from "../assets/jsMiniProj.png";
+import cssMini from "../assets/cssMiniProj.png";
+import digitalClock from "../assets/digitalClock.png";
 
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Imagify",
-        w_img:imagify,
-        link : "https://aiimagify.netlify.app/"
-    },
-    {
-        w_no:2,
-        w_name:"taxi booking",
-        w_img:taxi,
-        link: "https://ramnadtaxi.netlify.app/" 
-    },
-    {
-        w_no:3,
-        w_name:"portfolio",
-        w_img:portfolio,
-        link: "https://gowthamgportfolio.netlify.app/"
-    },
-    {
-        w_no:4,
-        w_name:"realestate",
-        w_img:realEstate,
-        link: "https://elitehomesearch.netlify.app/"
-    },
-    {
-        w_no:5,
-        w_name:"Currency Convertor",
-        w_img:project3_img,
-        link: "https://currencyconvertor2024.netlify.app/"
-    },  
-    { 
-        w_no:6,
-        w_name:"Weather App",
-        w_img:project1_img,
-        link : "https://weatherappgg.netlify.app/"
-    },
-   
-   
-]
- 
+  // Main Projects First
+  {
+    w_no: 1,
+    w_name: "Prescripto - Doctor Appointment",
+    w_img: doctor,
+    link: "https://prescriptogg.netlify.app",
+    githubLink: "https://github.com/gowtham-ggg/doctorAppointment"
+  },
+  {
+    w_no: 2,
+    w_name: "Imagify",
+    w_img: imagify,
+    link: "https://aiimagify.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/MERN-Projects/tree/master/imagify"
+  },
+  {
+    w_no: 3,
+    w_name: "Portfolio",
+    w_img: portfolio,
+    link: "https://gowthamgportfolio.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/My-Portfolio"
+  },
+  {
+    w_no: 4,
+    w_name: "Real Estate",
+    w_img: realEstate,
+    link: "https://elitehomesearch.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/real-estate"
+  },
+  {
+    w_no: 5,
+    w_name: "Currency Converter",
+    w_img: project3_img,
+    link: "https://currencyconvertor2024.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/react-mini-projects"
+  },
+  {
+    w_no: 6,
+    w_name: "Weather App",
+    w_img: project1_img,
+    link: "https://weatherappgg.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/react-mini-projects"
+  },
+
+  // Other Projects
+  {
+    w_no: 7,
+    w_name: "Taxi Booking",
+    w_img: taxi,
+    link: "https://ramnadtaxi.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/Travels-Landing-page"
+  },
+  {
+    w_no: 8,
+    w_name: "QR Code Generator",
+    w_img: qrgen,
+    link: "https://qrcodegenerator2024.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/react-mini-projects"
+  },
+  {
+    w_no: 9,
+    w_name: "BMI Calculator",
+    w_img: bmi,
+    link: "https://bmicalculator2024.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/react-mini-projects"
+  },
+  {
+    w_no: 10,
+    w_name: "Random Password Generator",
+    w_img: passGen,
+    link: "https://randompasswordgeneratorbygg.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/react-mini-projects"
+  },
+  {
+    w_no: 11,
+    w_name: "Digital Clock",
+    w_img: digitalClock,
+    link: "https://digitalclockbygg.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/react-mini-projects"
+  },
+  {
+    w_no: 12,
+    w_name: "Simple Profile Card",
+    w_img: simpleProfile,
+    link: "https://simpleprofilecard2024.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/react-mini-projects"
+  },
+  {
+    w_no: 13,
+    w_name: "Movie Rating App",
+    w_img: movieRating,
+    link: "https://mrggmoviesratingapp.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/WSA-mini-projects"
+  },
+  {
+    w_no: 14,
+    w_name: "JS Mini Projects",
+    w_img: jsminiProj,
+    link: "https://javascriptminiprojects.netlify.app",
+    githubLink: "https://github.com/gowtham-ggg/WSA-mini-projects"
+  },
+  {
+    w_no: 15,
+    w_name: "CSS Mini Projects",
+    w_img: cssMini,
+    link: "https://gowthamcsswsa.netlify.app/",
+    githubLink: "https://github.com/gowtham-ggg/WSA-mini-projects"
+  }
+];
+
 export default mywork_data;
--- src/components/My Work/MyWork.jsx
+++ src/components/My Work/MyWork.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import "./mywork.css"
-import theme_pattern from "../../assets/theme_pattern.svg"
-import mywork_Data from "../../assets/mywork_data.js"
-import arrow from "../../assets/arrow_icon.svg"
+import React, { useState } from 'react';
+import "./mywork.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import mywork_Data from "../../assets/mywork_data.js";
+import arrow from "../../assets/arrow_icon.svg";
 
 export const MyWork = () => {
+  const [showMore, setShowMore] = useState(false); 
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <div id='work' className='mywork'>
-        <div className="mywork-title">
-            <h1>My Latest Work</h1>
-            <img src={theme_pattern} alt="theme" />
-        </div>
-        <div className="mywork-container">
-            {mywork_Data.map((work, index)=>{
-                return <a key={index} href={work.link} target='blank'>
-                    <img  src= {work.w_img} alt = {work.w_name}/>
-                </a>
-            })}
-        </div>
-            <div className="mywork-showmore">
-                <p>Show More</p>
-                <img src={arrow} alt="arrow"  className='arrow'/>
+      <div className="mywork-title">
+        <h1>My Latest Work</h1>
+        <img src={theme_pattern} alt="theme" />
+      </div>
+      <div className="mywork-container">
+        {mywork_Data.slice(0, showMore ? mywork_Data.length : 6).map((work, index) => (
+          <div key={index} className="project-card">
+            <a href={work.link} target='_blank' rel="noopener noreferrer">
+              <img src={work.w_img} alt={work.w_name} />
+            </a>
+            <div>{work.w_name}</div>
+            <div className="project-info">
+              <div className="project-links">
+                <a href={work.githubLink} target='_blank' rel="noopener noreferrer">GitHub</a>
+                <a href={work.link} target='_blank' rel="noopener noreferrer">Live</a>
+              </div>
             </div>
+          </div>
+        ))}
+      </div>
+      <div className="mywork-showmore" onClick={toggleShowMore}>
+        <p>{showMore ? 'Show Less' : 'Show More'}</p>
+        <img src={arrow} alt="arrow" className='arrow' />
+      </div>
     </div>
-  )
-}
+  );
+};
